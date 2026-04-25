3fa77dbdfd64b674332f9d721ce1507674b356b1
Hamza Eskiadam <eskdmhmz@gmail.com>
2026-04-25 18:06:24 +0300

    Update Kullanici.java

--- LifeSync/src/com/lifesync/Kullanici.java
+++ LifeSync/src/com/lifesync/Kullanici.java
@@ -1,2 +1,42 @@
 package com.lifesync;
-public abstract class Kullanici {}
+
+public abstract class Kullanici {
+	private int kullaniciId;
+	private String adSoyad;
+	private String email;
+	private String sifreHash;
+	
+	public Kullanici() {}
+	
+	public Kullanici(int kullaniciID, String adSoyad, String email, String sifreHash) {
+		this.kullaniciId = kullaniciId;
+		this.adSoyad = adSoyad;
+		this.email = email;
+		this.sifreHash = sifreHash;
+	}
+	
+	public void profilGuncelle(String adSoyad, String email) {
+		this.adSoyad = adSoyad;
+		this.email = email;
+	}
+	
+	public int getKullaniciID() { return kullaniciID; }
+	public void setKullaniciID(int kullaniciID) {
+		this.kullaniciId = kullaniciID;
+	}
+	
+	public String getAdSoyad() { return adSoyad; }
+	public void setAdSoyad(String adSoyad) {
+		this.adSoyad = adSoyad;
+	}
+	
+	public String getEmail() { return email; }
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getSifreHash() { return sifreHash; }
+	public void setSifreHash(String sifreHash) {
+		this.sifreHash = sifreHash;
+	}
+}
