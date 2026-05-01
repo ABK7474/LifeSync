5f6822616ba92a5d0401d3a4e11d5f84f91789c5
fatihozturk-1 <fatihozturk435@gmail.com>
2026-05-01 17:51:13 +0300

    Supplement sınıfı güncellendi

--- LifeSync/src/com/lifesync/Supplement.java
+++ LifeSync/src/com/lifesync/Supplement.java
@@ -2,13 +2,9 @@ package com.lifesync;
 public class Supplement implements IBilgiGetirebilir{
   
 	private int supplementId;
-	
 	private String supplementAdi;
-	
 	private double miktar;
-	
 	private Birim birim;
-	
 	private KullanimZamani kullanimZamani;
 
 	Supplement(int supplementId,String supplementAdi,double miktar,Birim birim,KullanimZamani kullanimZamani)
@@ -21,61 +17,21 @@ public class Supplement implements IBilgiGetirebilir{
 	}
 	
 	//region Getters and Setters
-
-	
-
-	public int getSupplementId() {
-		return supplementId;
-	}
-
-	public void setSupplementId(int supplementId) {
-		this.supplementId = supplementId;
-	}
-
-	public String getSupplementAdi() {
-		return supplementAdi;
-	}
-
-	public void setSupplementAdi(String supplementAdi) {
-		this.supplementAdi = supplementAdi;
-	}
-
-	public double getMiktar() {
-		return miktar;
-	}
-
-	public void setMiktar(double miktar) {
-		this.miktar = miktar;
-	}
-
-	public Birim getBirim() {
-		return birim;
-	}
-
-	public void setBirim(Birim birim) {
-		this.birim = birim;
-	}
-
-	public KullanimZamani getKullanimZamani() {
-		return kullanimZamani;
-	}
-
-	public void setKullanimZamani(KullanimZamani kullanimZamani) {
-		this.kullanimZamani = kullanimZamani;
-	}
-	
+	public int getSupplementId() { return supplementId; }
+	public void setSupplementId(int supplementId) { this.supplementId = supplementId; }
+	public String getSupplementAdi() { return supplementAdi; }
+	public void setSupplementAdi(String supplementAdi) { this.supplementAdi = supplementAdi; }
+	public double getMiktar() { return miktar; }
+	public void setMiktar(double miktar) { this.miktar = miktar; }
+	public Birim getBirim() { return birim; }
+	public void setBirim(Birim birim) { this.birim = birim; }
+	public KullanimZamani getKullanimZamani() { return kullanimZamani; }
+	public void setKullanimZamani(KullanimZamani kullanimZamani) { this.kullanimZamani = kullanimZamani; }
 	//endregion
 	
-	public String bilgiGetir()
-	{
+	public String bilgiGetir(){
 		return  "Supplement Adi: " + supplementAdi +
 				", Miktar :" + miktar + " " + birim +
 				", Kullanim Zamani: " + kullanimZamani;
 	}
-	
 }
-
-
-
-
-
