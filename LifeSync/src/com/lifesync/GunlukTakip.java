6a2c121002165fa99c95a4c76655e0c177d92edc
fatihozturk-1 <fatihozturk435@gmail.com>
2026-05-01 18:06:56 +0300

    GunlukTakip sınıfından kod satırını azaltmak için düzenleme kod içerisinde bir değişiklik yok

--- LifeSync/src/com/lifesync/GunlukTakip.java
+++ LifeSync/src/com/lifesync/GunlukTakip.java
@@ -8,13 +8,9 @@ import java.util.List;
 public class GunlukTakip implements IOzetlenebilir{
   
 	private int takipId;
-	
 	private LocalDate tarih;
-	
 	private double suMiktari;
-	
 	private List<Supplement> supplementListesi;
-	
 	private String gunlukNot;
 	
 	Birim litre= Birim.LITRE;
@@ -30,47 +26,16 @@ public class GunlukTakip implements IOzetlenebilir{
 	
 	
 	//region Getters and Settters
-
-	public int getTakipId() {
-		return takipId;
-	}
-
-	public void setTakipId(int takipId) {
-		this.takipId = takipId;
-	}
-
-	public LocalDate getTarih() {
-		return tarih;
-	}
-
-	public void setTarih(LocalDate tarih) {
-		this.tarih = tarih;
-	}
-
-	public double getSuMiktari() {
-		return suMiktari;
-	}
-
-	public void setSuMiktari(double suMiktari) {
-		this.suMiktari = suMiktari;
-	}
-
-	public List<Supplement> getSupplementListesi() {
-		return supplementListesi;
-	}
-
-	public void setSupplementListesi(List<Supplement> supplementListesi) {
-		this.supplementListesi = supplementListesi;
-	}
-
-	public String getGunlukNot() {
-		return gunlukNot;
-	}
-
-	public void setGunlukNot(String gunlukNot) {
-		this.gunlukNot = gunlukNot;
-	}
-	
+	public int getTakipId() { return takipId; }
+	public void setTakipId(int takipId) { this.takipId = takipId; }
+	public LocalDate getTarih() { return tarih; }
+	public void setTarih(LocalDate tarih) { this.tarih = tarih; }
+	public double getSuMiktari() { return suMiktari; }
+	public void setSuMiktari(double suMiktari) { this.suMiktari = suMiktari; }
+	public List<Supplement> getSupplementListesi() { return supplementListesi; }
+	public void setSupplementListesi(List<Supplement> supplementListesi) { this.supplementListesi = supplementListesi; }
+	public String getGunlukNot() { return gunlukNot; }
+	public void setGunlukNot(String gunlukNot) { this.gunlukNot = gunlukNot; }
 	//endregion
 	
 	public void gunlukNotEkle(String not)
