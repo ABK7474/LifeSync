8fa0859c2ad52f99b74b0a9de08a0136b863bc14
fatihozturk-1 <fatihozturk435@gmail.com>
2026-05-01 18:02:46 +0300

    Besin sınıfından kod satırını azaltmak için düzenleme kod içerisinde bir değişiklik yok

--- LifeSync/src/com/lifesync/Besin.java
+++ LifeSync/src/com/lifesync/Besin.java
@@ -1,22 +1,16 @@
 package com.lifesync;
 public class Besin implements IBilgiGetirebilir{
-	private int besinId;
-
-	private String besinAdi;
-
-	private double kalori;
 	
+	private int besinId;
+	private String besinAdi;
+	private double kalori;
 	private double protein;
-
 	private double karbonhidrat;
-
 	private double yag;
-
 	private double lif;
 	
 	Birim gram = Birim.GRAM;
 	Birim kcal = Birim.KCAL;
-	
 	
 	Besin(int besinId,String besinAdi,double kalori,double protein,double karbonhidrat,double yag,double lif)
 	{
@@ -30,63 +24,20 @@ public class Besin implements IBilgiGetirebilir{
 	}
 
 	//region Getters and Setters
-	
-	public int getBesinId() {
-		return besinId;
-	}
-
-	public void setBesinId(int besinId) {
-		this.besinId = besinId;
-	}
-
-	public String getBesinAdi() {
-		return besinAdi;
-	}
-
-	public void setBesinAdi(String besinAdi) {
-		this.besinAdi = besinAdi;
-	}
-
-	public double getKalori() {
-		return kalori;
-	}
-
-	public void setKalori(double kalori) {
-		this.kalori = kalori;
-	}
-
-	public double getProtein() {
-		return protein;
-	}
-
-	public void setProtein(double protein) {
-		this.protein = protein;
-	}
-
-	public double getKarbonhidrat() {
-		return karbonhidrat;
-	}
-
-	public void setKarbonhidrat(double karbonhidrat) {
-		this.karbonhidrat = karbonhidrat;
-	}
-
-	public double getYag() {
-		return yag;
-	}
-
-	public void setYag(double yag) {
-		this.yag = yag;
-	}
-
-	public double getLif() {
-		return lif;
-	}
-
-	public void setLif(double lif) {
-		this.lif = lif;
-	}
-
+	public int getBesinId() { return besinId; }
+	public void setBesinId(int besinId) { this.besinId = besinId; }
+	public String getBesinAdi() { return besinAdi; }
+	public void setBesinAdi(String besinAdi) { this.besinAdi = besinAdi; }
+	public double getKalori() { return kalori; }
+	public void setKalori(double kalori) { this.kalori = kalori; }
+	public double getProtein() { return protein; }
+	public void setProtein(double protein) { this.protein = protein; }
+	public double getKarbonhidrat() { return karbonhidrat; }
+	public void setKarbonhidrat(double karbonhidrat) { this.karbonhidrat = karbonhidrat; }
+	public double getYag() { return yag; }
+	public void setYag(double yag) { this.yag = yag; }
+	public double getLif() { return lif; }
+	public void setLif(double lif) { this.lif = lif; }
 	//endregion
 	
 	public String bilgiGetir()
@@ -98,6 +49,4 @@ public class Besin implements IBilgiGetirebilir{
 				", Yag Miktari: " + yag + " " + gram +
 				", Lif Miktari: " + lif + " " + gram;
 	}
-	
-	
 }
