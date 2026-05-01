d4d664039a6f3ee1d0ae653029244a68a2d3571a
fatihozturk-1 <fatihozturk435@gmail.com>
2026-05-01 17:24:41 +0300

    java.util.* düzeltmesi

--- LifeSync/src/com/lifesync/GunlukTakip.java
+++ LifeSync/src/com/lifesync/GunlukTakip.java
@@ -1,7 +1,8 @@
 package com.lifesync;
 
 import java.time.LocalDate;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class GunlukTakip implements IOzetlenebilir{
