d16c7c738b292e45d660ba3cb2bf0bfcef2f65a9
fatihozturk-1 <fatihozturk435@gmail.com>
2026-05-01 17:22:02 +0300

    Update Antrenor

--- LifeSync/src/com/lifesync/Antrenor.java
+++ LifeSync/src/com/lifesync/Antrenor.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Antrenor extends Kullanici{
     private List<Sporcu> sporcuListesi;
 
-     // sporcuListesi'ni boş bir ArrayList olarak başlatır.
+     // sporcuListesi'ni boş bir ArrayList olarak başlatır nesne oluşturulduğunda
     public Antrenor(int kullaniciId, String adSoyad, String email, String sifreHash) {
         super(kullaniciId, adSoyad, email, sifreHash);
         this.sporcuListesi = new ArrayList<>();
@@ -20,22 +20,21 @@ public class Antrenor extends Kullanici{
     // HENÜZ SPORCU SINIFI YAZILMADIĞI İÇİN YORUM SATIRI OLARAK YAZILMIŞTIR
 
     // Mantıksal operasyon: Belirtilen antrenmanı, belirtilen sporcunun antrenman listesine ekler.
-    // public void programAta(Sporcu sporcu, Antrenman antrenman) {
-    //     if (sporcu != null && antrenman != null) {
-    //         sporcu.antrenmanEkle(antrenman);
-    //     } else {
-    //         throw new IllegalArgumentException("Sporcu veya Antrenman referansı null (boş) olamaz.");
-    //     }
-    // }
-
+    public void programAta(Sporcu sporcu, Antrenman antrenman) {
+        if (sporcu != null && antrenman != null) {
+            sporcu.antrenmanEkle(antrenman);
+        } else {
+            throw new IllegalArgumentException("Sporcu veya Antrenman referansı null (boş) olamaz.");
+        }
+    }
     // Mantıksal operasyon: Sporcunun verilerini analiz edip string formatında rapor döner.
-    // public String ilerlemeIncele(Sporcu sporcu) {
-    //     if (sporcu != null) {
-    //         // İleride veritabanından çekilecek verilerle doldurulacak iş mantığı alanı
-    //         return "Sporcu: " + sporcu.getAdSoyad() + " için ilerleme analizi tamamlandı.";
-    //     }
-    //     return "Geçersiz sporcu referansı.";
-    // }
+    public String ilerlemeIncele(Sporcu sporcu) {
+        if (sporcu != null) {
+            // İleride veritabanından çekilecek verilerle doldurulacak iş mantığı alanı
+            return "Sporcu: " + sporcu.getAdSoyad() + " için ilerleme analizi tamamlandı.";
+        }
+        return "Geçersiz sporcu referansı.";
+    }
 
     // Encapsulation (Kapsülleme) gereği private değişken için dışarıdan kontrollü erişim sağlayan Setter metodu
     public void setSporcuListesi(List<Sporcu> sporcuListesi) {
